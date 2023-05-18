82c0964d947903058702a9b3d0f65d94fa5e7338
tomaspm98 <up202006845@up.pt>
2023-05-18 13:14:49 +0100

    Trying to play again with same account at the end( not working)

--- assign2/src/Server.java
+++ assign2/src/Server.java
@@ -3,13 +3,19 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.*;
 import java.util.concurrent.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.List;
+import java.util.Map;
 
 public class Server {
     private static final int PORT = 12345;
     private static final int MAX_GAMES = 5;
     private static final int PLAYERS_PER_GAME = 2;
     private static final int MAX_WAIT_TIME = 60000;
-    private static final int MAX_LEVEL_DIFFERENCE = 6;  // Changed from 10 to 7
+    private static final int MAX_LEVEL_DIFFERENCE = 6;  
 
     public static void main(String[] args) {
         ExecutorService gameExecutor = Executors.newFixedThreadPool(MAX_GAMES);
@@ -24,7 +30,7 @@ public class Server {
                         User user = new User(socket);
                         int authStatus = user.authenticate();
                         if (authStatus == 1) {
-                        waitingUsers.put(user, System.currentTimeMillis());
+                            waitingUsers.put(user, System.currentTimeMillis());
                     } else if (authStatus == -1) {
                         user.close();
                         }
@@ -80,10 +86,12 @@ public class Server {
                                             Game game = new Game(PLAYERS_PER_GAME, userSockets);
                                             try {
                                                 Socket winnerSocket = gameExecutor.submit(game::start).get();
-
+                                                askForAnotherGame(players, waitingUsers);
+                    
                                                 if (winnerSocket == null) {
                                                     for (User player : players) {
                                                         player.setLevel(player.getLevel() + 3);
+                                                        //checkForAnotherGame(player, waitingUsers);
                                                     }
                                                 } else {
                                                     for (User player : players) {
@@ -92,6 +100,7 @@ public class Server {
                                                         } else {
                                                             player.setLevel(player.getLevel() + 1);
                                                         }
+                                                        //checkForAnotherGame(player, waitingUsers);
                                                     }
                                                 }
                                             } catch (InterruptedException | ExecutionException e) {
@@ -121,5 +130,26 @@ public class Server {
             gameExecutor.shutdown();
     }
 }
+
+    private static void askForAnotherGame(List<User> players, Map<User,Long> waitingUsers) {
+        for (User player : players) {
+            try {
+                PrintWriter out = new PrintWriter(player.getSocket().getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(player.getSocket().getInputStream()));
+
+                out.println("Do you want to play another game? (yes/no)");
+                out.flush();
+
+                String response = in.readLine();
+                if (response != null && response.equalsIgnoreCase("yes")) {
+                    waitingUsers.put(player, System.currentTimeMillis());
+                } else {
+                    player.getSocket().close();
+                }
+            } catch (IOException e) {
+                System.err.println("Error in askForAnotherGame: " + e.getMessage());
+            }
+        }
+    }
 }
 
